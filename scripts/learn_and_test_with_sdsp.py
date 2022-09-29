8a3031bd401dc4bb8137fb3c6f19dbd49e61b390
Yakir <yakir4123@gmail.com>
2022-09-29 08:17:53 +0300

    Work on learning neuron using stdp

--- scripts/learn_and_test_with_sdsp.py
+++ scripts/learn_and_test_with_sdsp.py
@@ -142,19 +142,8 @@ def test_neurons(freqs, audio):
 
 
 if __name__ == '__main__':
-    freqs = [
-        (100, 3, 299),
-        (250, 4, 60),
-        (500, 5, 14),
-        (1000, 6, 3),
-        (1750, 6, 1),
-        (2800, 3, 10),
-        (3500, 3, 8),
-        (5000, 3, 4),
-        (7500, 4, 1),
-        (10000, 3, 2),
-        ]
-    # learn_neurons(freqs)
+    freqs = [int(100 * (1.18 ** i)) for i in range(0, 23)]
+    learn_neurons(freqs)
     # test_neurons(freqs, 'bells5')
     # test_neurons(freqs, 'bottle1')
     # test_neurons(freqs, 'buzzer')
